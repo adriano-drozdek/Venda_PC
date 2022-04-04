5a87e73a1068ab8db48ea875615a1a042b5b2ad8
Adriano Drozdek <adrianodrozdek2791@gmail.com>
2022-04-04 15:21:13 -0300

    Criado entidade ITEM.

---
+++ src/entities/Item.java
@@ -0,0 +1,33 @@
+package entities;
+
+public class Item {
+
+	private Integer quantidade;
+	private Double preco;
+
+	public Item() {
+
+	}
+
+	public Item(Integer quantidade, Double preco) {
+		this.quantidade = quantidade;
+		this.preco = preco;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+}
