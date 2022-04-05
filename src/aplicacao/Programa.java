77a56840c4715a78e3d0ec14f9848ecdb411f68f
Adriano Drozdek <adrianodrozdek2791@gmail.com>
2022-04-05 14:02:06 -0300

    Instanciando ITEM e imprimindo.

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,12 +1,17 @@
 package aplicacao;
 
+import entities.Item;
 import entities.Produto;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		Produto p = new Produto("TV ", 1000.00);
+		Produto p = new Produto("TV ", 1000.0);
+		
+		Item it1 = new Item(1, 1000.0, p);
+		
+		System.out.println(it1.getProduto().getNome());
 	}
 
 }
