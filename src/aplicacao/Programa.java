7a026e0b03db8ec1ea040d03a17ddba0c53d2db4
Adriano Drozdek <adrianodrozdek2791@gmail.com>
2022-04-06 13:33:49 -0300

    Instanciando cliente com os dados para digitar.

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,17 +1,33 @@
 package aplicacao;
 
-import entities.Item;
-import entities.Produto;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Cliente;
 
 public class Programa {
 
-	public static void main(String[] args) {
-		
-		Produto p = new Produto("TV ", 1000.0);
-		
-		Item it1 = new Item(1, 1000.0, p);
-		
-		System.out.println(it1.getProduto().getNome());
+	public static void main(String[] args) throws ParseException {
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Entre com os dados do cliente: ");
+		System.out.print("Nome: ");
+		String nome = sc.nextLine();
+		System.out.print("Email: ");
+		String email = sc.next();
+		System.out.println("Data de nascimento: ");
+		Date dataNasc = sdf.parse(sc.next());
+
+		Cliente cliente = new Cliente(nome, email, dataNasc);
+		sc.close();
+
 	}
 
 }
